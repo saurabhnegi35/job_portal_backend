c28e8527f42a8fb614b3d79e849ee54a50ab46d9
saurabhnegi35 <69843862+saurabhnegi35@users.noreply.github.com>
2025-05-23 08:40:58 +0530

    cors origin added

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://job-portal-frontend-phi-eight.vercel.app/",
+      "https://job-portal-frontend-phi-eight.vercel.app",
     ],
     credentials: true,
   })
