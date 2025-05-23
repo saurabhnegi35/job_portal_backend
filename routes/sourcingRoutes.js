c50b65f3e4e3c1f0ef67d323aefba23e4c4e1575
saurabhnegi35 <69843862+saurabhnegi35@users.noreply.github.com>
2025-05-23 08:48:20 +0530

    Given access to admin to search jobseekers

--- routes/sourcingRoutes.js
+++ routes/sourcingRoutes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const { authenticate } = require("../middlewares/authMiddleware");
-const { isHR } = require("../middlewares/roleMiddleware");
+const { isHR, isAdminOrHR } = require("../middlewares/roleMiddleware");
 const sourcingController = require("../controllers/sourcingController");
 
-router.get("/jobseekers", isHR, sourcingController.searchJobSeekers);
-router.post("/source/:jobId/:userId", isHR, sourcingController.sourceCandidate);
+router.get("/jobseekers", isAdminOrHR, sourcingController.searchJobSeekers);
+router.post(
+  "/source/:jobId/:userId",
+  isAdminOrHR,
+  sourcingController.sourceCandidate
+);
 router.get(
   "/sourced/:jobId",
   isHR,
